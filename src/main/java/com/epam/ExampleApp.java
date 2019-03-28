b158372f6a493a4c3444a5dff93ed19ae28922a5
thatglance <11sashamak@gmail.com>
2019-03-29 00:40:00 +0300

    remove unnecessary code

--- src/main/java/com/epam/ExampleApp.java
+++ src/main/java/com/epam/ExampleApp.java
@@ -1,14 +1,7 @@
 package main.java.com.epam;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-import java.util.function.Function;
-
 import main.java.com.epam.api.Path;
 import main.java.com.epam.api.GpsNavigator;
-import main.java.com.epam.graph.entity.Edge;
-import main.java.com.epam.graph.entity.Vertex;
 import main.java.com.epam.impl.GpsNavigatorImpl;
 
 /**
@@ -16,19 +9,7 @@ import main.java.com.epam.impl.GpsNavigatorImpl;
  */
 public class ExampleApp {
 
-    public static void main(String[] args) {
-
-        List<Edge> persons = new ArrayList<Edge>();
-//        Collections.sort
-        /*Collections.sort(persons, new Comparator<Person>() {
-            public int compare(Person person, Person person1) {
-                int name = person.getName().compareTo(person1.getName());
-                if(name == 0){
-                    return name;
-                }
-                return person.getAge() > person1.getAge() ? 1 : person.getAge() < person1.getAge() ? -1 : 0;
-            }
-        });*/
+    public static void main(final String[] args) {
 
         final GpsNavigator navigator = new GpsNavigatorImpl();
         navigator.readData("input.txt");
